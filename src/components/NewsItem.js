df344deeec272e376b49329c408800a86c5e8705
urvi <urvisharma54@gmail.com>
2022-07-01 22:58:46 +0530

    fetched data

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,13 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imgUrl} className="card-img-top" alt="..." />
+          <img src={!imgUrl?"https://static.toiimg.com/thumb/msid-92591751,width-1070,height-580,imgsize-27896,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg":imgUrl} 
+               className="card-img-top"
+               alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-primary btn-sm">Read More</a>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-dark btn-sm">Read More</a>
           </div>
         </div>
       </div>
