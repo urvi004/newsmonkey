05d30cc91302161ac2db0d4eaa038aca38cc3571
urvi <urvisharma54@gmail.com>
2022-07-02 10:30:17 +0530

    added spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
 
 
 export class News extends Component {
@@ -16,11 +17,17 @@ export class News extends Component {
 
   async componentDidMount() {
     console.log("cdm")
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3e0fa9c6396e4b76b1fc594bca5f952d&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3e0fa9c6396e4b76b1fc594bca5f952d
+               &page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData)
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    this.setState({ 
+                    articles: parsedData.articles, 
+                    totalResults: parsedData.totalResults,
+                    loading:false
+                   })
 
 
   }
@@ -29,43 +36,42 @@ export class News extends Component {
     console.log("prev");
 
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3e0fa9c6396e4b76b1fc594bca5f952d&page=
-               ${this.state.page - 1}&pageSize=20`;
+               ${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+   this.setState({loading:true})
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData)
 
     this.setState({
       page: this.state.page - 1,
-      articles: parsedData.articles
+      articles: parsedData.articles,
+      loading: false
     })
   }
 
   handleNextClick = async () => {
     console.log("Next");
-    if(this.state.page + 1> Math.ceil(this.state.totalResults/20)){
-
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3e0fa9c6396e4b76b1fc594bca5f952d&page=
+      ${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true})
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading:false
+      })
     }
-    else{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3e0fa9c6396e4b76b1fc594bca5f952d&page=
-               ${this.state.page + 1}&pageSize=20`;
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData)
-
-    this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles
-    })
-  }
   }
 
   render() {
     return (
       <div className='container my-3'>
-        <h1>NewsMonkey- top headline</h1>
-
+        <h1 className='text-center'>NewsMonkey- top headline</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return <div className="col md-4" key={element.url}>
               <NewsItem title={element.title ? element.title.slice(0, 45) : (" ")}
                 description={element.description ? element.description.slice(0, 88) : (" ")}
@@ -76,7 +82,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
           <button disabled={this.state.page <= 1} type="button" className='btn btn-dark' onClick={this.handlePrevClick}> &larr; Previous</button>
-          <button  type="button" className='btn btn-dark' onClick={this.handleNextClick}> Next &rarr;</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className='btn btn-dark' onClick={this.handleNextClick}> Next &rarr;</button>
         </div>
       </div>
     )
