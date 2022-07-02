60b60a35412be56bdd25b4bbca559f1e43cee9bc
urvi <urvisharma54@gmail.com>
2022-07-02 15:36:11 +0530

    Routing

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+
 
 
 export class News extends Component {
